9875fd332f82c722e46d8a526d3aaf23b898d8dd
yuelele <1120036574@qq.com>
2025-04-08 20:49:37 +0800

    首次实验

---
+++ src/com/itheima/speedkey/SpeedKeyDemo.java
@@ -0,0 +1,9 @@
+package com.itheima.speedkey;
+
+public class SpeedKeyDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello world2");
+        System.out.println("Hello world3");
+        System.out.println("Hello world1");
+    }
+}
